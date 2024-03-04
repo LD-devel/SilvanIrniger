623f40cbbefb3d3dc9ab85892e5b023b634c705b
unknown <laszlo.dirks@rwth-aachen.de>
2024-03-04 12:54:19 +0100

    Fixed form routing.

--- backend/main.py
+++ backend/main.py
@@ -7,17 +7,40 @@ from typing import Optional
 
 app = FastAPI()
 
-app.mount("/", StaticFiles(directory="../frontend", html=True), name="static")
+app.mount("/assets", StaticFiles(directory="../frontend/assets", html=True), name="static")
+app.mount("/images", StaticFiles(directory="../frontend/images", html=True), name="static")
+app.mount("/utils", StaticFiles(directory="../frontend/utils", html=True), name="static")
+
+@app.get("/")
+async def get_index():
+    return FileResponse("../frontend/index.html")
+
+@app.get("/about")
+async def get_about():
+    return FileResponse("../frontend/about.html")
+
+@app.get("/concerts")
+async def get_concerts():
+    return FileResponse("../frontend/concerts.html")
+
+@app.get("/contact")
+async def get_contact():
+    return FileResponse("../frontend/contact.html")
+
+@app.get("/gallery")
+async def get_gallery():
+    return FileResponse("../frontend/gallery.html")
 
 class ContactForm(BaseModel):
     name: str
     email: str
     subject: str
-    message: Optional[str] = None
+    message: str
 
 @app.post("/submit-contact-form/")
 async def submit_form(contact_form: ContactForm):
     try:
+        print(contact_form)
         return {"message": "Form submission successful"}
     except Exception as e:
         raise HTTPException(status_code=500, detail="Internal server error")
